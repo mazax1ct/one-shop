2b24566b1d534d695b2d159a787a5d144d494a54
mazax1ct <vova@giveandget.ru>
2020-05-20 15:08:53 +0300

    main-page adaptive

--- src/js/main.js
+++ src/js/main.js
@@ -1,13 +1,16 @@
 //открытие/закрытие главного меню
+
+var targetElement = $('.header__menus');
+
 $(document).on('click', '.js-menu-toggler', function () {
   $(this).toggleClass("is-active");
   $('.header__menus').toggleClass('is-open');
   $('.header__user-link').toggle();
   $('.header__user-links-separate').toggle();
-  /*if($(this).hasClass('is-active')) {
+  if($(this).hasClass('is-active')) {
     bodyScrollLock.disableBodyScroll(targetElement);
   } else {
     bodyScrollLock.enableBodyScroll(targetElement);
-  }*/
+  }
   return false;
 });
