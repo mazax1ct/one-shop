8dba25bec20c79c98485d3b4e5cb81a246aa27b8
mazax1ct <vova@giveandget.ru>
2020-06-01 14:28:31 +0300

    checkout

--- build/js/main.js
+++ build/js/main.js
@@ -20,6 +20,110 @@ function equalHeight(selector){
 	$(selector).height(tallest);
 }
 
+//расширение для select2
+(function($) {
+
+	var Defaults = $.fn.select2.amd.require('select2/defaults');
+
+  $.extend(Defaults.defaults, {
+  	dropdownPosition: 'auto'
+  });
+
+ 	var AttachBody = $.fn.select2.amd.require('select2/dropdown/attachBody');
+
+  var _positionDropdown = AttachBody.prototype._positionDropdown;
+
+  AttachBody.prototype._positionDropdown = function() {
+
+    var $window = $(window);
+
+		var isCurrentlyAbove = this.$dropdown.hasClass('select2-dropdown--above');
+		var isCurrentlyBelow = this.$dropdown.hasClass('select2-dropdown--below');
+
+		var newDirection = null;
+
+		var offset = this.$container.offset();
+
+		offset.bottom = offset.top + this.$container.outerHeight(false);
+
+		var container = {
+    		height: this.$container.outerHeight(false)
+		};
+
+    container.top = offset.top;
+    container.bottom = offset.top + container.height;
+
+    var dropdown = {
+      height: this.$dropdown.outerHeight(false)
+    };
+
+    var viewport = {
+      top: $window.scrollTop(),
+      bottom: $window.scrollTop() + $window.height()
+    };
+
+    var enoughRoomAbove = viewport.top < (offset.top - dropdown.height);
+    var enoughRoomBelow = viewport.bottom > (offset.bottom + dropdown.height);
+
+    var css = {
+      left: offset.left,
+      top: container.bottom
+    };
+
+    // Determine what the parent element is to use for calciulating the offset
+    var $offsetParent = this.$dropdownParent;
+
+    // For statically positoned elements, we need to get the element
+    // that is determining the offset
+    if ($offsetParent.css('position') === 'static') {
+      $offsetParent = $offsetParent.offsetParent();
+    }
+
+    var parentOffset = $offsetParent.offset();
+
+    css.top -= parentOffset.top
+    css.left -= parentOffset.left;
+
+    var dropdownPositionOption = this.options.get('dropdownPosition');
+
+		if (dropdownPositionOption === 'above' || dropdownPositionOption === 'below') {
+
+    		newDirection = dropdownPositionOption;
+
+    } else {
+
+        if (!isCurrentlyAbove && !isCurrentlyBelow) {
+      			newDirection = 'below';
+    		}
+
+    		if (!enoughRoomBelow && enoughRoomAbove && !isCurrentlyAbove) {
+      		newDirection = 'above';
+    		} else if (!enoughRoomAbove && enoughRoomBelow && isCurrentlyAbove) {
+      		newDirection = 'below';
+    		}
+
+    }
+
+    if (newDirection == 'above' ||
+        (isCurrentlyAbove && newDirection !== 'below')) {
+      css.top = container.top - parentOffset.top - dropdown.height;
+    }
+
+    if (newDirection != null) {
+      this.$dropdown
+        .removeClass('select2-dropdown--below select2-dropdown--above')
+        .addClass('select2-dropdown--' + newDirection);
+      this.$container
+        .removeClass('select2-container--below select2-container--above')
+        .addClass('select2-container--' + newDirection);
+    }
+
+    this.$dropdownContainer.css(css);
+
+  };
+
+})(window.jQuery);
+
 $(document).ready(function () {
   //запуск функции навешивания класса на шапку
   resize_scroll();
@@ -29,10 +133,20 @@ $(document).ready(function () {
     equalHeight('.catalog-description td');
   }
 
-  //кастомный скролл
+  //кастомный скролл для контейнера таблиц
   $('.js-custom-scroll').each(function(index, element) {
     new SimpleBar(element, { autoHide: false })
   });
+
+  //кастомный селект
+	$('.js-select').each(function() {
+   var $p = $(this).closest('.select-wrapper');
+   $(this).select2({
+		 minimumResultsForSearch: Infinity,
+     dropdownPosition: 'below',
+     dropdownParent: $p
+   });
+	});
 
   //слайдер картинок
   if ($('.js-slider').length) {
@@ -73,7 +187,7 @@ $(document).ready(function () {
     });
   }
 
-  //запуск плавающего левого меню в разделе доставки
+  //запуск плавающего левого меню на контентных страницах
   if ($(".js-sticky-block").length) {
     if ($("body").width() >= 1280) {
       $(".js-sticky-block").trigger("sticky_kit:detach");
